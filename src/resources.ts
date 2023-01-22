f391c3aa906018016f87d2b10116e2dc72535b0a
yactouat <yactouat@hotmail.com>
2023-01-22 16:50:37 +0100

    `BuildResource`

--- src/resources.ts
+++ src/resources.ts
@@ -1,5 +1,38 @@
-const dummyResource = {
-  name: "dummy",
-};
+export interface BuildResource {
+  aliasAssigned: number | null;
+  aliasError: string | null;
+  buildingAt: number | null;
+  created: number | null;
+  createdAt: number | null;
+  creator: {
+    email: string;
+    githubLogin: string;
+    uid: string;
+    username: string;
+  } | null;
+  inspectorUrl: string;
+  isRollbackCandidate: boolean | null;
+  meta: {
+    githubCommitAuthorLogin: string;
+    githubCommitAuthorName: string;
+    githubCommitMessage: string;
+    githubCommitOrg: string;
+    githubCommitRef: string;
+    githubCommitRepo: string;
+    githubCommitRepoId: string;
+    githubCommitSha: string;
+    githubDeployment: string;
+    githubOrg: string;
+    githubRepo: string;
+    githubRepoId: string;
+    githubRepoOwnerType: string;
+  };
+  name: string;
+  ready: number;
+  state: BuildStateType;
+  type: string;
+  uid: string;
+  url: string;
+}
 
-export default dummyResource;
+export type BuildStateType = "ERROR" | "READY" | "MASKED";
