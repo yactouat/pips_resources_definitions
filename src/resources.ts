2bef7bed070db92524bf889e0e098d424604d7bc
yactouat <yactouat@hotmail.com>
2023-01-27 23:28:09 +0100

    a Vercel deployment is not a resource

--- src/resources.ts
+++ src/resources.ts
@@ -11,43 +11,6 @@ export interface BlogPostResource {
   title: string;
 }
 
-export interface BuildResource {
-  aliasAssigned: number | null;
-  aliasError: string | null;
-  buildingAt: number | null;
-  created: number | null;
-  createdAt: number | null;
-  creator: {
-    email: string;
-    githubLogin: string;
-    uid: string;
-    username: string;
-  } | null;
-  inspectorUrl: string;
-  isRollbackCandidate: boolean | null;
-  meta: {
-    githubCommitAuthorLogin: string;
-    githubCommitAuthorName: string;
-    githubCommitMessage: string;
-    githubCommitOrg: string;
-    githubCommitRef: string;
-    githubCommitRepo: string;
-    githubCommitRepoId: string;
-    githubCommitSha: string;
-    githubDeployment: string;
-    githubOrg: string;
-    githubRepo: string;
-    githubRepoId: string;
-    githubRepoOwnerType: string;
-  };
-  name: string;
-  ready: number;
-  state: BuildResourceStateType;
-  type: string;
-  uid: string;
-  url: string;
-}
-
 export interface ImageResource {
   alt: string;
   b64: string;
--- src/types.ts
+++ src/types.ts
@@ -13,3 +13,40 @@ export type CRUDResourceType =
   | "User";
 
 export type SocialHandleType = "GitHub" | "LinkedIn";
+
+export interface VercelDeploymentType {
+  aliasAssigned: number | null;
+  aliasError: string | null;
+  buildingAt: number | null;
+  created: number | null;
+  createdAt: number | null;
+  creator: {
+    email: string;
+    githubLogin: string;
+    uid: string;
+    username: string;
+  } | null;
+  inspectorUrl: string;
+  isRollbackCandidate: boolean | null;
+  meta: {
+    githubCommitAuthorLogin: string;
+    githubCommitAuthorName: string;
+    githubCommitMessage: string;
+    githubCommitOrg: string;
+    githubCommitRef: string;
+    githubCommitRepo: string;
+    githubCommitRepoId: string;
+    githubCommitSha: string;
+    githubDeployment: string;
+    githubOrg: string;
+    githubRepo: string;
+    githubRepoId: string;
+    githubRepoOwnerType: string;
+  };
+  name: string;
+  ready: number;
+  state: BuildResourceStateType;
+  type: string;
+  uid: string;
+  url: string;
+}
